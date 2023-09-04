94dad26312cbfcfba1f7c679ce93e34c50c2f208
rncraven94 <neilcraven2013@gmail.com>
2023-09-04 18:31:50 -0400

    script.js

--- script.js
+++ script.js
@@ -2,8 +2,26 @@ use: "strict";
 
 const brownSetter = document.getElementById("brown-setter");
 let brownSetterNumber = 7;
+const greenSetter = document.getElementById("green-setter");
+let greenSetterNumber = 7;
+const silverSetter = document.getElementById("silver-setter");
+let silverSetterNumber = 7;
+const blueSetter = document.getElementById("blue-setter");
+let blueSetterNumber = 7;
+const redSetter = document.getElementById("red-setter");
+let redSetterNumber = 7;
 
 brownSetter.addEventListener("click", function () {
   brownSetterNumber--;
   brownSetter.textContent = brownSetterNumber;
+});
+
+greenSetter.addEventListener("click", function () {
+  greenSetterNumber--;
+  greenSetter.textContent = greenSetterNumber;
+});
+
+silverSetter.addEventListener("click", function () {
+  silverSetterNumber--;
+  silverSetter.textContent = silverSetterNumber;
 });
