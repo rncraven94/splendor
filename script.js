128c69412584a7778146737b095cd2015f1eebc0
rncraven94 <neilcraven2013@gmail.com>
2023-09-04 18:18:21 -0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 use: "strict";
+
+const brownSetter = document.getElementById("brown-setter");
+let brownSetterNumber = 7;
+
+brownSetter.addEventListener("click", function () {
+  brownSetterNumber--;
+  brownSetter.textContent = brownSetterNumber;
+});
